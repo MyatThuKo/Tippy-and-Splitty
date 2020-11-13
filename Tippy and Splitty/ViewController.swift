6c80ca0b3d7612f614062e89545b5f43fb071b05
Myat Thu Ko <nymyatthuko@gmail.com>
2020-11-13 00:23:12 -0500

    Finished

--- Tippy and Splitty/ViewController.swift
+++ Tippy and Splitty/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipPercentage: UILabel!
+    @IBOutlet weak var totalAmount: UILabel!
+    @IBOutlet weak var billAmount: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        let bill = Double(billAmount.text!) ?? 0
+        
+        let tipPercentages = [0.15,0.18,0.20,0.25]
+        
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipPercentage.text = String(format: "$%.2f", tip)
+        
+        totalAmount.text = String(format: "$%.2f", total)
+    }
+    
 }
 
