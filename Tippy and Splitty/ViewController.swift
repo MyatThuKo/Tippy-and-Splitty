a975427d9b8fedec7956c83a2bfde9c66f4e7d93
Myat Thu Ko <nymyatthuko@gmail.com>
2020-11-16 09:57:37 -0500

    Added UserDefaults

--- Tippy and Splitty/ViewController.swift
+++ Tippy and Splitty/ViewController.swift
@@ -9,16 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var tipPercentage: UILabel!
+    @IBOutlet weak var tipAmount: UILabel!
     @IBOutlet weak var totalAmount: UILabel!
     @IBOutlet weak var billAmount: UITextField!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var totalPerPerson: UILabel!
     @IBOutlet weak var numberOfPeopleLabel: UITextField!
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        numberOfPeopleLabel.text = String(defaults.integer(forKey: "numOfPeople"))
+        tipControl.selectedSegmentIndex = defaults.integer(forKey: "tipPercent")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("View will appear.")
+        defaults.set(2, forKey: "tipPercent")
+        defaults.set(1, forKey: "numOfPeople")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("View did appear.")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("View will disappear.")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("View did disappear.")
     }
 
     @IBAction func onTap(_ sender: Any) {
@@ -35,7 +59,7 @@ class ViewController: UIViewController {
         let total = bill + tip
         let numberOfPeople = Int(numberOfPeopleLabel.text!) ?? 1
         
-        tipPercentage.text = String(format: "$%.2f", tip)
+        tipAmount.text = String(format: "$%.2f", tip)
         
         totalAmount.text = String(format: "$%.2f", total)
         
